b66569c8cfa55328a5da515c728630bcc8d92f7b
owenn <nigelowen2@gmail.com>
2023-04-03 23:03:32 +1200

    have something working at least, stage 1, pulls list of ships down

---
+++ src/routes/showShips/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from "svelte";
+    import { apiData, shipNames } from './store.js';
+    
+    onMount(async () => {
+      fetch("http://jane:5000/v1/ships")
+      .then(response => response.json())
+      .then(data => {
+            console.log(data);     
+        apiData.set(data);
+      }).catch(error => {
+        console.log(error);
+        return [];
+      });
+    });
+</script>
+    
+    <main>
+        <h1>Ship names</h1>
+        <ul>
+        {#each $shipNames as shipName}
+            <li>{shipName}</li>
+        {/each}
+        </ul>
+    </main>
+    
+    <style>
+    
+    </style>
